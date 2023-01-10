3d76380f9fd01eb3ffddbb04bcc70667600d214b
Helio Nunes <helioaugustofn@protonmail.com>
2023-01-10 01:57:14 -0400

    Logic and UI finished

--- pedraPapelTesoura.js
+++ pedraPapelTesoura.js
@@ -16,63 +16,63 @@ function playRound(playerSelection) {
     if (playerSelection === 'Pedra' && computerSelection === 'Tesoura' ||
         playerSelection === 'Papel' && computerSelection === 'Pedra' ||
         playerSelection === 'Tesoura' && computerSelection === 'Papel') {
-        showResults(`Você ganhou! ${playerSelection} ganha de ${computerSelection}`);
-        return 'v';
+        showResults(`Você ganhou! ${playerSelection} ganha de ${computerSelection}. ${evaluateScore('v')}`);
     } else if (playerSelection === computerSelection) {
-        showResults('Empatou!');
-        return 'e';
+        showResults(`Empatou. ${evaluateScore('e')}`);
     } else {
-        showResults(`Você perdeu :( ${computerSelection} ganha de ${playerSelection}`);
-        return 'd';
+        showResults(`Você perdeu :( ${computerSelection} ganha de ${playerSelection}. ${evaluateScore('d')}`);
     }
 }
 
 function printEndGameMessage(playerScore, cpuScore) {
     if (playerScore === cpuScore) {
-        return `O placar final terminou empatado ${playerScore}x${cpuScore}`;
+        return `Placar final: Empate ${playerScore}x${cpuScore}`;
     } else if (playerScore > cpuScore) {
-        return `Jogador ganhou de ${playerScore}x${cpuScore}`;
+        return `Placar final: Jogador ganhou de ${playerScore}x${cpuScore}`;
     } else {
-        return `CPU ganhou de ${cpuScore}x${playerScore}`;
+        return `Placar final: CPU ganhou de ${cpuScore}x${playerScore}`;
     }
 }
 
 function evaluateScore(result) {
+    timesPlayed++;
     if (result === 'v') {
         playerScore++;
     } else if (result === 'd') {
         cpuScore++;
     }
 
-    timesPlayed++;
-
     if (timesPlayed === 5) {
-        showResults(printEndGameMessage(playerScore, cpuScore));
+        let finalResult = printEndGameMessage(playerScore, cpuScore);
         timesPlayed = 0;
         playerScore = 0;
         cpuScore = 0;
+        return finalResult;
     }
+
+    return `Placar: ${playerScore}x${cpuScore}`;
 }
 
-function playGame(playerSelection) {   
-    let result = playRound(playerSelection.target.value);  
-    evaluateScore(result);
+function playGame(playerSelection) {
+    playRound(playerSelection.target.value);
 }
+
 
 function showResults(resultMessage) {
-    const result = document.createElement('div');
     result.textContent = resultMessage;
-    result.classList.toggle('result');
-    document.body.appendChild(result);
 }
 
 let playerScore = 0,
     cpuScore = 0,
-    timesPlayed = 0;
+    timesPlayed = 0,
+    timesToReset = 0;
+
+// <div> that is updated with showResults function
+const result = document.createElement('div');
+result.classList.toggle('result');
+document.body.appendChild(result);
 
 const options = document.querySelectorAll('button');
-
 options.forEach(button => {
     button.addEventListener('click', playGame);
 });
-
